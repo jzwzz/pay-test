a28f61741cb42fc5f9a233188e4250d33c24cc30
zengzhou wang <jzwzz@163.com>
2018-11-24 11:19:56 +0800

    first commit

---
+++ src/main/java/com/example/paytest/PayTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.paytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayTestApplication.class, args);
+    }
+}
